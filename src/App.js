bb3365a3b09c44731826906d5042173a4d1ff57f
Richard Davis <rdavis.bacs@gmail.com>
2017-02-13 17:55:34 -0500

    working code

--- src/App.js
+++ src/App.js
@@ -1,58 +1,73 @@
 import React, { Component } from 'react'
-import SearchGitHubProfile from './SearchGitHubProfile'
-import GitHubProfile from './GitHubProfile'
+import SearchBox from './SearchBox'
+import Profile from './Profile'
 import './App.css'
 
-const API = 'https://api.github.com/users';
+const API = 'https://api.github.com/users'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       username: 'hesmaili',
-      name:'',
-      avatar:'',
-      location:'',
-      repos:'',
+      name: '',
+      avatar: '',
+      location: '',
+      repos: '',
       followers: '',
-      following:'',
-      homeUrl:'',
-      notFound:''
+      following: '',
+      homeUrl: '',
+      notFound: ''
     }
+    this.fetchProfile = this.fetchProfile.bind(this)
   }
 
   fetchProfile(username) {
-    let url = `${API}/${username}`;
+    let url = `${API}/${username}`
     fetch(url)
-      .then((res) => res.json() )
-      .then((data) => {
-        this.setState({
-          username: data.login,
-          name: data.name,
-          avatar: data.avatar_url,
-          location: data.location,
-          repos: data.public_repos,
-          followers: data.followers,
-          following: data.following,
-          homeUrl: data.html_url,
-          notFound: data.message
-        })
+    .then((response) => {
+      if (response.statusCode === 200) {
+        return response.json()
+      } else {
+        throw new Error(response.statusText)
+      }
+    })
+    .then((json) => {
+      this.setState({
+        username: json.login,
+        name: json.name,
+        avatar: json.avatar_url,
+        location: json.location,
+        repos: json.public_repos,
+        followers: json.followers,
+        following: json.following,
+        homeUrl: json.html_url,
+        notFound: json.message
       })
-      .catch((error) => console.log('Oops! . There Is A Problem') )
+    })
+    .catch((error) => {
+      console.log('Oops! There was a problem.')
+      console.log(error)
+    })
   }
 
   componentDidMount() {
-    this.fetchProfile(this.state.username);
+    this.fetchProfile(this.state.username)
   }
 
   render() {
     return (
       <div>
-         <section id="card">
-           <SearchGitHubProfile fetchProfile={this.fetchProfile.bind(this)}/>
-           <GitHubProfile data={this.state} />
-         </section>
-          <span className="hesmaili">GitHub Card With ReactJs - Created By <a href="https://twitter.com/hesmaili95" target="_blank" title="Hamed Esmaili">Hamed Esmaili</a></span>
+        <section id="card">
+          <SearchBox fetchProfile={this.fetchProfile} />
+          <Profile data={this.state} />
+        </section>
+        <span className="creator">
+          GitHub Card With ReactJs - Created By
+          <a href="https://twitter.com/hesmaili95" target="_blank" title="Hamed Esmaili">
+            Hamed Esmaili
+          </a>
+        </span>
       </div>
     )
   }
---
+++ src/SearchBox.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class SearchBox extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { username: '' }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({username: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.fetchProfile(this.state.username)
+    this.setState({ username: '' })
+  }
+
+  render() {
+    return (
+      <div className="search--box">
+        <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
+          <label>
+            <input type="search" name="username" placeholder="Username" value={this.state.username} />
+          </label>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SearchBox
