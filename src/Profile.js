3ae7b4ee3e08b6d3dd8c777c1db3e96f3ef3ad6f
Richard Davis <rdavis.bacs@gmail.com>
2018-10-23 13:13:12 -0400

    refactor profile into functional component witout state

--- src/Profile.js
+++ src/Profile.js
@@ -1,61 +1,59 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Profile extends Component {
-  render() {
-    if (!this.props.data.found) {
-      return (
-        <div className="not-found">
-          <h2>Oops!</h2>
-          <p>We could not find a GitHub user with that username. Try again.</p>
-        </div>
-      )
-    }
-
-    let data = this.props.data
-    let followers = `${data.homeUrl}/followers`
-    let repositories = `${data.homeUrl}?tab=repositories`
-    let following = `${data.homeUrl}/following`
-
+const Profile = (props) => {
+  if (!props.data.found) {
     return (
-      <section className="github--profile">
-        <div className="github--profile__info">
-          <a href={data.homeUrl} title={data.name || data.username}>
-            <img src={data.avatar} alt={data.username}/>
-          </a>
-
-          <h2>
-            <a href={data.homeUrl} title={data.username}>
-              {data.name || data.username}
-            </a>
-          </h2>
-          <h3>{data.location}</h3>
-        </div>
-
-        <div className="github--profile__state">
-          <ul>
-             <li>
-                <a href={followers} title="Number Of Followers">
-                  <i>{data.followers}</i>
-                  <span> Followers</span>
-                </a>
-             </li>
-             <li>
-                <a href={repositories} title="Number Of Repositoriy">
-                  <i>{data.repos}</i>
-                  <span> Repositories</span>
-                </a>
-             </li>
-             <li>
-                <a href={following} title="Number Of Following">
-                  <i>{data.following}</i>
-                  <span> Following</span>
-                </a>
-             </li>
-          </ul>
-        </div>
-      </section>
+      <div className="not-found">
+        <h2>Oops!</h2>
+        <p>We could not find a GitHub user with that username. Try again.</p>
+      </div>
     )
   }
+
+  let data = props.data
+  let followers = `${data.homeUrl}/followers`
+  let repositories = `${data.homeUrl}?tab=repositories`
+  let following = `${data.homeUrl}/following`
+
+  return (
+    <section className="github--profile">
+      <div className="github--profile__info">
+        <a href={data.homeUrl} title={data.name || data.username}>
+          <img src={data.avatar} alt={data.username}/>
+        </a>
+
+        <h2>
+          <a href={data.homeUrl} title={data.username}>
+            {data.name || data.username}
+          </a>
+        </h2>
+        <h3>{data.location}</h3>
+      </div>
+
+      <div className="github--profile__state">
+        <ul>
+           <li>
+              <a href={followers} title="Number Of Followers">
+                <i>{data.followers}</i>
+                <span> Followers</span>
+              </a>
+           </li>
+           <li>
+              <a href={repositories} title="Number Of Repositories">
+                <i>{data.repos}</i>
+                <span> Repositories</span>
+              </a>
+           </li>
+           <li>
+              <a href={following} title="Number Of Following">
+                <i>{data.following}</i>
+                <span> Following</span>
+              </a>
+           </li>
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export default Profile
